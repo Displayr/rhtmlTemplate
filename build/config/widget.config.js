997de9a85a41c1a2a8e712ca36d4ec15676682a0
Kyle Zeeuwen <kyle.zeeuwen@gmail.com>
2017-11-01 08:11:07 +1000

    VIS-364: use rhtmlBuildUtils 2.0.0

--- build/config/widget.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  widgetEntryPoint: 'theSrc/scripts/rhtmlTemplate.js',
-  widgetName: 'rhtmlTemplate',
-  visualRegressionSuite: {
-    applitools: {
-      defaultMatchTimeout: 2
-    }
-  }
-}
