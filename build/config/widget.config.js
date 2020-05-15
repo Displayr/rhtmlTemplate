22894cbd1c7421a100685c89d91131272d5bb99d
Kyle Zeeuwen <kyle.zeeuwen@gmail.com>
2020-05-15 17:16:59 +1000

    use rhtmlBuildUtils 5.0.0

---
+++ build/config/widget.config.js
@@ -0,0 +1,24 @@
+const cliArgs = require('yargs').argv
+const _ = require('lodash')
+
+const config = {
+  widgetEntryPoint: 'theSrc/scripts/rhtmlTemplate.js',
+  widgetFactory: 'theSrc/scripts/rhtmlTemplate.factory.js',
+  widgetName: 'rhtmlTemplate',
+  internalWebSettings: {
+    includeDimensionsOnWidgetDiv: true,
+    default_border: false,
+    css: [],
+    isReadySelector: 'svg[rhtmlTemplate-status=ready]',
+    singleWidgetSnapshotSelector: '#widget-container'
+  },
+  snapshotTesting: {
+    consoleLogHandler: () => {}, // suppress logging in snapshot testing
+    testplanDirectory: 'theSrc/test/snapshotTestDefinitions',
+    interactionTestDirectory: 'theSrc/test/bin',
+    snapshotDirectory: 'theSrc/test/snapshots'
+  }
+}
+
+const commandLineOverides = _.omit(cliArgs, ['_', '$0'])
+module.exports = _.merge(config, commandLineOverides)
