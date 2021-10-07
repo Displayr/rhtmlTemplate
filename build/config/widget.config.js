1084457058f103409bea6be60497ea91a9ce3776
Justin Yap <JustinCCYap@users.noreply.github.com>
2021-10-07 16:16:27 +1100

    Revert "VIS-928: standardise widget status attribute"
    This reverts commit 563b82f2444527dad032b58966cb84faa8cce45c.

--- build/config/widget.config.js
+++ build/config/widget.config.js
@@ -9,7 +9,7 @@ const config = {
     includeDimensionsOnWidgetDiv: true,
     default_border: false,
     css: [],
-    isReadySelector: 'div[rhtmlwidget-status=ready]',
+    isReadySelector: 'svg[rhtmlTemplate-status=ready]',
     singleWidgetSnapshotSelector: '#widget-container',
   },
   snapshotTesting: {
--- theSrc/scripts/Template.js
+++ theSrc/scripts/Template.js
@@ -63,9 +63,9 @@ class Template {
     this._clearRootElement()
     this._manipulateRootElementSize()
     this._addRootSvgToRootElement()
-    this.outerSvg.attr(`rhtmlwidget-status`, 'loading')
+    this.outerSvg.attr(`rhtmlTemplate-status`, 'loading')
     this._draw()
-    this.outerSvg.attr(`rhtmlwidget-status`, 'ready')
+    this.outerSvg.attr(`rhtmlTemplate-status`, 'ready')
   }
 
   _clearRootElement () {
