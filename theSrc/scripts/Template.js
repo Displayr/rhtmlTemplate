04bcfdb13fed76040c3f736f94f69acaf69ff797
Justin Yap <JustinCCYap@users.noreply.github.com>
2021-10-12 09:35:24 +1100

    VIS-928: standardise widget status attribute

--- theSrc/scripts/Template.js
+++ theSrc/scripts/Template.js
@@ -63,9 +63,9 @@ class Template {
     this._clearRootElement()
     this._manipulateRootElementSize()
     this._addRootSvgToRootElement()
-    this.outerSvg.attr(`rhtmlTemplate-status`, 'loading')
+    this.rootElement.setAttribute(`rhtmlwidget-status`, 'loading')
     this._draw()
-    this.outerSvg.attr(`rhtmlTemplate-status`, 'ready')
+    this.rootElement.setAttribute(`rhtmlwidget-status`, 'ready')
   }
 
   _clearRootElement () {
